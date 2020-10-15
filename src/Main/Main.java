e3d42427776f19d40219fa4e37751285998ac5d2
Marcel Prielinger <marcel.prielinger@htl-wels.at>
2020-10-15 10:24:38 +0200

    Added Buttons, Texts and Lables

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,8 +2,10 @@ package Main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /*******************************
@@ -21,11 +23,32 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        GridPane root = new GridPane();
+        VBox root = new VBox();
 
+        Label celsiusText = new Label("Celsius");
+        Label fahrenheitText = new Label("Fahrenheit");
 
+        TextArea celsius = new TextArea();
+        celsius.setMaxHeight(1);
+        celsius.setMaxWidth(100);
 
-        Scene scene = new Scene(root, 300, 635);
+        TextArea fahrenheit = new TextArea();
+        fahrenheit.setMaxHeight(1);
+        fahrenheit.setMaxWidth(100);
+
+        Button c_to_f = new Button("Celsius zu Fahrenheit");
+        Button f_to_c = new Button("Fahrenheit zu Celsius");
+
+        root.getChildren().add(celsiusText);
+        root.getChildren().add(celsius);
+
+        root.getChildren().add(fahrenheitText);
+        root.getChildren().add(fahrenheit);
+
+        root.getChildren().add(c_to_f);
+        root.getChildren().add(f_to_c);
+
+        Scene scene = new Scene(root, 200, 200);
         stage.setTitle("Memory");
         stage.setScene(scene);
         stage.show();
