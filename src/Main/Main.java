4568c6dfb6cf1254c3339787146b8258ac4ca3f5
Marcel Prielinger <marcel.prielinger@htl-wels.at>
2020-10-15 10:02:01 +0200

    Initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,35 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+/*******************************
+ * @author Marcel Prielinger
+ * @version 1 / 20.4.2020
+ *******************************/
+
+public class Main extends Application {
+
+    public static void main(String[] args)
+    {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        GridPane root = new GridPane();
+
+
+
+        Scene scene = new Scene(root, 300, 635);
+        stage.setTitle("Memory");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
